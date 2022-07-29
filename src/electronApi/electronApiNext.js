b17231da1ba8474a44c98597f10d4c88326a0351
raulito <raul_0.4_@hotmail.com>
2022-07-29 18:11:15 +0200

    Better aproach in electronApi

--- src/electronApi/electronApiNext.js
+++ src/electronApi/electronApiNext.js
@@ -1,72 +1,33 @@
-//START DEFINE ENDPOINTS
+// -------------------------------------------------------------------------------------------
 
-const { SELECT_DIRECTORY } = require("./service/actions.js");
-const {
-  GET_USER_DATA,
-  SAVE_USER_DATA,
-} = require("./service/electronStoreService");
-const {
-  GENERATOR_LIST,
-  ADD_GENERATOR,
-  DELETE_GENERATOR,
-  GET_GENERATOR,
-  GENERATE_STRUCTURE_GENERATOR,
-  UPDATE_GENERATOR,
-} = require("./controllers/generatorController");
+//ALL ENDPOINTS
 
-const endpoints = [
-  {
-    name: "SELECT_DIRECTORY",
-    function: SELECT_DIRECTORY,
-  },
-  {
-    name: "GET_USER_DATA",
-    function: GET_USER_DATA,
-  },
-  {
-    name: "SAVE_USER_DATA",
-    function: SAVE_USER_DATA,
-  },
-  { name: "GENERATOR_LIST", function: GENERATOR_LIST },
-  {
-    name: "ADD_GENERATOR",
-    function: ADD_GENERATOR,
-  },
-  {
-    name: "DELETE_GENERATOR",
-    function: DELETE_GENERATOR,
-  },
-  {
-    name: "GET_GENERATOR",
-    function: GET_GENERATOR,
-  },
-  {
-    name: "GENERATE_STRUCTURE_GENERATOR",
-    function: GENERATE_STRUCTURE_GENERATOR,
-  },
-  {
-    name: "UPDATE_GENERATOR",
-    function: UPDATE_GENERATOR,
-  },
-];
+const endpointsMerge = {
+  ...require("./controllers/generatorController"),
+  ...require("./service/actions"),
+  ...require("./service/electronStoreService"),
+};
 
-//FINISH DEFINE ENDPOINTS
+//ALL ENDPOINTS
 
 // -------------------------------------------------------------------------------------------
 
-// FOR USE IN preload.js example:
+const endpointsName = Object.keys(endpointsMerge);
+
+// TO USE IN preload.js example:
 // TEST_NAME = (data = {}) => ipcRenderer.invoke('TEST_NAME', data);
 const preloadEndpoints = (ipcRenderer) =>
-  endpoints.reduce((previusEndpointList, newEndpoint) => {
-    previusEndpointList[newEndpoint.name] = (data = {}) =>
-      ipcRenderer.invoke(newEndpoint.name, data);
+  endpointsName.reduce((previusEndpointList, newEndpoint) => {
+    previusEndpointList[newEndpoint] = (data = {}) =>
+      ipcRenderer.invoke(newEndpoint, data);
     return previusEndpointList;
   }, []);
 
-// FOR USE IN background.js
+// TO USE IN background.js
+// ipcMain.handle("endpointName", function);
 const dynamicImportElectronaApi = (ipcMain) =>
-  endpoints.map((endpoint) => {
-    ipcMain.handle(endpoint.name, endpoint.function);
+  endpointsName.map((endpoint) => {
+    ipcMain.handle(endpoint, endpointsMerge[endpoint]);
   });
 
-module.exports = { preloadEndpoints, endpoints, dynamicImportElectronaApi };
+module.exports = { preloadEndpoints, dynamicImportElectronaApi };
